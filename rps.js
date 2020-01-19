142d16145fa938c492333a7bc8efaf324163539b
tmcnutt22 <thomas.mcnutt.jr@gmail.com>
2020-01-18 21:57:42 -0500

    updated code

--- rps.js
+++ rps.js
@@ -2,50 +2,49 @@
 var wins = 0;
 var losses = 0;
 
-//declaration of variables
+// declaration of variables
 r = document.getElementById("rock").onclick;
 p = document.getElementById("paper").onclick;
 s = document.getElementById("scissors").onclick;
 
-//Player choice
-function playerSelection(rock, paper, scissors);
- playerChoice = playerChoice.onclick();
-    
-}
+// Player choice
+function playerSelection(rock, paper, scissors)
+    var playerSelection= r;
 
 //computer random choice
-play[r,p,s];
+var play = ['r','p','s'];
 function computerSelection(play) {
-    computerPlay = play[Math.floor(Math.random() * play.length-1)];
-        return (play);
-    
+    computerPlay = Math.floor.play(Math.random.play() * play.length];
+        return (computerPlay);  
  }
+   
+
 
  //play round
  function playRound(playerSelection, computerSelection) {
         var i;
         for (i = 0; wins < 3 || losses < 3; i++){
             while (playerSelection == computerSelection);{
-                if (playerSelection = r && computerSelection = s){
+                if (playerSelection = r && computerSelection == s){
                     wins = wins++;
                 }
-                else if (playerSelection = r && computerSelection = p) {
+                else if (playerSelection = r && computerSelection == p) {
                     losses = losses++;
                 }
-                else if (playerSelection = p && computerSelection = r){
+                else if (playerSelection = p && computerSelection == r){
                     wins = wins++;
                 }
-                else if(playerSelection = p && computerSelection = s){
+                else if(playerSelection = p && computerSelection == s){
                     losses = losses++;
                 }
-                else if(playerSelection = s && computerSelection = p){
+                else if(playerSelection = s && computerSelection ==p){
                     wins = wins++;
                 }
-                else if(playerSelection = s && computerSelection = r){
+                else if(playerSelection = s && computerSelection == r){
                     losses = losses++;
                 }
             }
-            else break;
+            break;
         }   
 }
 
